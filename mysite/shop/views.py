5837874e627879953844e83af1d22c1ee5e320c0
Aditya Verma <aadityavermaa@gmail.com>
2020-07-01 21:33:08 +0530

    Ecom

---
+++ mysite/shop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return render(request, 'shop/about.html') 
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+def search(request):
+    return render(request, 'shop/search.html')   
+
+def productview(request):
+    return render(request, 'shop/productview.html')
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
